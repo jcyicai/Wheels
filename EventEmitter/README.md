82259a9491a45702f8f09d8ea5b6e1880f50583a
chenyicai <chenyicai622@qq.com>
2022-08-02 14:17:54 +0800

    feat(事件模型): eventEmitter文档编写

---
+++ EventEmitter/README.md
@@ -0,0 +1,112 @@
+# EventEmitter
+
+## 介绍
+一个简单的事件模型，实现事件的订阅与发布
+
+
+
+## 使用
+```html
+<script src="../eventEmitter.js"></script>
+```
+
+
+
+## API
+```javascript
+const emitter = new EventEmitter()
+```
+
+#### on
+
+添加事件，支持链式调用
+
+```javascript
+/**
+ * @param {String} eventName 事件名称
+ * @param {Function} listener 事件回调
+ */
+emitter.on(eventName, listener)
+```
+
+#### once
+
+添加事件，该事件仅被执行一次，支持链式调用
+
+```javascript
+/**
+ * @param {String} eventName 事件名称
+ * @param {Function} listener 事件回调
+ */
+emitter.once(eventName, listener)
+```
+
+#### off
+
+删除某一事件的监听器，支持链式调用
+
+```javascript
+/**
+ * @param {String} eventName 事件名称
+ * @param {Function} listener 事件回调
+ */
+emitter.off(eventName, listener)
+```
+
+#### allOff
+
+删除某一事件的所有监听器
+
+```javascript
+/**
+ * @param {String} eventName 事件名称
+ */
+emitter.allOff(eventName)
+```
+
+#### emit
+
+触发事件，支持链式调用
+
+```javascript
+/**
+ * @param {String} eventName 事件名称
+ * @param {Array} args 监听器参数
+ */
+emitter.emit(eventName, ...args)
+```
+
+
+
+## 示例
+
+#### 添加、触发、删除事件
+
+```javascript
+// 创建实例
+const emitter = new EventEmitter()
+
+// cb监听器
+const cb = function (a, b) {
+    console.log(a + b)
+}
+
+// foo监听器
+const foo = function () {
+    console.log('foo')
+}
+
+// 先注册 再执行 再移除
+emitter.on('click', cb).emit('click', 1, 2).off('click', cb)
+
+emitter.on('blur', foo) // 注册blur事件 添加foo监听器
+emitter.emit('blur') // 执行blur事件
+emitter.off('blur', foo) // 移除blur事件的 foo监听器
+```
+
+
+
+## 参考
+
+[EventEmitter.js](https://github.com/Olical/EventEmitter)
+[Wheels-EventEmitter.js](https://github.com/mqyqingfeng/EventEmitter)
--- EventEmitter/eventEmitter.js
+++ EventEmitter/eventEmitter.js
@@ -1,11 +1,11 @@
 class EventEmitter {
     constructor() {
-        // 存放注册的事件和回调
+        // 存放注册的事件和事件监听器
         this._events = {}
     }
 
     /**
-     * @description: 验证回调是否是函数
+     * @description: 验证监听器是否是函数
      * @return {Boolean}
      */
     isValidListener(listener) {
@@ -19,9 +19,9 @@ class EventEmitter {
     }
 
     /**
-     * @description: 查找回调数组中是否包含此回调
-     * @param {Array} listeners 回调数组
-     * @param {Function} listener 回调
+     * @description: 查找监听器数组中是否包含此监听器
+     * @param {Array} listeners 监听器数组
+     * @param {Function} listener 监听器函数
      * @return {Number} 如果没有找到 返回 -1
      */
     indexOfListener(listeners, listener) {
@@ -40,26 +40,26 @@ class EventEmitter {
     /**
      * @description: 添加事件
      * @param {String} eventName 事件名称
-     * @param {Function} listener 事件回调
+     * @param {Function} listener 监听器函数
      * @return {Object} 可链式调用
      */
     on(eventName, listener) {
         if (!eventName || !listener) {
-            // 事件名称 或 事件回调 其中一个不存  直接 return
+            // 事件名称 或 监听器 其中一个不存  直接 return
             return
         }
 
         if (!this.isValidListener(listener)) {
-            // 事件回调必须是个函数
+            // 监听器必须是个函数
             throw new TypeError('listener must be a function')
         }
 
-        let events = this._events  // 事件合集
-        let listeners = events[eventName] = events[eventName] || []  // 事件对应的回调合集
-        let listenerIsWrapped = typeof listener === 'object' // 判断回调是否是对象
+        const events = this._events  // 事件合集
+        const listeners = events[eventName] = events[eventName] || []  // 事件对应的监听器合集
+        const listenerIsWrapped = typeof listener === 'object' // 判断监听器是否是对象
 
         if (this.indexOfListener(listeners, listener) === -1) {
-            // 不重复添加回调
+            // 不重复添加监听器
             listeners.push(listenerIsWrapped ? listener : {
                 listener,
                 once: false
@@ -71,8 +71,8 @@ class EventEmitter {
 
     /**
      * @description: 添加事件，该事件仅被执行一次
-     * @param {eventName} 事件名称
-     * @param {listener} 事件回调
+     * @param {String} eventName 事件名称
+     * @param {Function} listener 监听器函数
      * @return {Object} 可链式调用
      */
     once(eventName, listener) {
@@ -80,9 +80,9 @@ class EventEmitter {
     }
 
     /**
-     * @description: 删除事件
-     * @param {eventName} 事件名称
-     * @param {listener} 事件回调
+     * @description: 删除某一事件的监听器
+     * @param {String} eventName 事件名称
+     * @param {Function} listener 监听器函数
      * @return {Object} 可链式调用
      */
     off(eventName, listener) {
@@ -91,7 +91,7 @@ class EventEmitter {
             return
         }
 
-        // 遍历当前事件回调合集  如果匹配到，直接删除
+        // 遍历当前监听器合集  如果匹配到，直接删除
         for (let i = 0; i < listeners.length; i++) {
             let item = listeners[i]
             if (item && item.listener === listener) {
@@ -104,8 +104,8 @@ class EventEmitter {
     }
 
     /**
-     * @description: 删除某一事件的所有回调
-     * @param {eventName} 事件名称
+     * @description: 删除某一事件的所有监听器
+     * @param {String} eventName 事件名称
      */
     allOff(eventName) {
         if (eventName && this._events[eventName]) {
@@ -118,7 +118,7 @@ class EventEmitter {
     /**
      * @description: 事件触发
      * @param {String} eventName 事件名称
-     * @param {Array} args 事件回调参数
+     * @param {Array} args 监听器参数
      * @return {Object} 可链式调用
      */
     emit(eventName, ...args) {
@@ -132,7 +132,7 @@ class EventEmitter {
             if (item) {
                 item.listener(...args)
                 if (item.once) {
-                    // 如果该回调只执行一次，则执行完后 移除该回调
+                    // 如果该监听器只执行一次，则执行完后 移除该监听器
                     this.off(eventName, item.listener)
                 }
             }
