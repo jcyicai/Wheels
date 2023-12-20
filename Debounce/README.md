547130a1dc31b0e699527a16271eb7c4f171a361
chenyicai <chenyicai622@qq.com>
2023-12-20 13:57:05 +0800

    feat(debounce): update debounce

--- Debounce/README.md
+++ Debounce/README.md
@@ -14,15 +14,21 @@
  * @description: 防抖
  * @param {Function} func 执行函数
  * @param {Number} wait 延迟时间
+ * @param {Boolean} immediate 立即执行
  * @return {Function}
  */
-debounce(func, wait)
+debounce(func, wait, immediate)
 ```
 
 ## 示例
 
 ```javascript
-var app = document.getElementById('app');
-app.onmousemove = debounce(func, wait)
+var app = document.getElementById('app')
+var btn = document.getElementById('btn')
+var action = debounce(func, wait, immediate)
+app.onmousemove = action
+btn.addEventListener('click', function () {
+    action.cancel() // 取消防抖
+})
 ```
 
--- Debounce/debounce.js
+++ Debounce/debounce.js
@@ -2,16 +2,43 @@
  * @description: 防抖
  * @param {Function} func 执行函数
  * @param {Number} wait 延迟时间
+ * @param {Boolean} immediate 立即执行
  * @return {Function}
  */
-function debounce(func, wait) {
-    var timer;
-    return function () {
-        var context = this
-        var args = arguments
-        clearTimeout(timer)
-        timer = setTimeout(function () {
-            func.apply(context, args)
-        }, wait)
+function debounce(func, wait, immediate) {
+  var timer, result
+
+  var debounced = function () {
+    var context = this
+    var args = arguments
+
+    if (timer) {
+      clearTimeout(timer)
     }
+
+    if (immediate) {
+      var callNow = !timer
+
+      timer = setTimeout(function () {
+        timer = null
+      }, wait)
+
+      if (callNow) {
+        result = func.apply(context, args)
+      }
+    } else {
+      timer = setTimeout(function () {
+        func.apply(context, args)
+      }, wait)
+    }
+
+    return result
+  }
+  // 取消防抖
+  debounced.cancel = function () {
+    clearTimeout(timer)
+    timer = null
+  }
+
+  return debounced
 }
